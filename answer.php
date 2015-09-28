bf4008bff0aea0ff6b041aebd204ba5f07fc3265
Jinpeiqi <626335649@qq.com>
2015-09-28 19:05:18 +1300

    Add Validation for quiz and Add Answer.php

--- answer.php
+++ answer.php
@@ -1,1 +1,52 @@
-Answer
+<?php
+    $total=0;
+    if($_POST["Q1"]==3){
+        $total++;
+    }else{
+        echo  "Question 1 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q2"]==4){
+        $total++;
+    }else{
+        echo  "Question 2 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q3"]==2){
+        $total++;
+    }else{
+        echo  "Question 3 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q4"]==3){
+        $total++;
+    }else{
+        echo  "Question 4 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q5"]==3){
+        $total++;
+    }else{
+        echo  "Question 5 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q6"]==2){
+        $total++;
+    }else{
+        echo  "Question 6 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q7"]==4){
+        $total++;
+    }else{
+        echo  "Question 7 wrong";
+        echo "</br>";
+    }
+    if($_POST["Q8"]==2){
+        $total++;
+    }else{
+        echo  "Question 8 wrong";
+        echo "</br>";
+    }
+    echo "Your total score is ",$total;
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JINPEIQI
+ * Date: 2015/9/28
+ * Time: 10:45
+ */
