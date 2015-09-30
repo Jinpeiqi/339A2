14adc02be57632b5c2b30c8d02b30bcd02ad9689
Jinpeiqi <626335649@qq.com>
2015-09-30 19:55:28 +1300

    CSS

--- index.php
+++ index.php
@@ -9,6 +9,14 @@
             #P_quiz{
                 text-align: center;
             }
+            #Home_info{
+                width: 450px;
+                height: 25px;
+                border: 3px solid #8AC007;
+                background: #66ed86;
+                font-size: 20px;
+                color: #4a64ed;
+            }
         </style>
     </head>
     <body>
@@ -17,5 +25,6 @@
     <div id="button"> <a href="index.php"><input type="button" value="Home"></a>
      <a href="login.php"><input type="button" value="Log in"></a>
      <a href="signup.php"><input type="button" value="Sign up"></a></div>
+    <p id="Home_info">=>You must be logged in to view this page.</p>
     </body>
 </html>
