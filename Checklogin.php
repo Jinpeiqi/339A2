25b30a2b36b02a2aae8531e686686aeef6ec5b19
Jinpeiqi <626335649@qq.com>
2015-09-30 14:26:22 +1300

    Add CSS and Pic in index.php

---
+++ Checklogin.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+
+if ($username&&$password){
+    require_once 'function.php';
+    $result=queryMysql("SELECT * FROM member WHERE username='$username' and password='$password'");
+    if($result->num_rows==0){
+        echo 'Username/Password invalid';
+    }else{
+        $_SESSION['username']=$username;
+    }
+}else{
+    die("Please enter and username and a password");
+}
+?>
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <html>
     <head>
+        <style>
+            #title{
+                color: #d068ff;
+                background-color: #ffb489;
+                text-align: center;
+            }
+            #P_quiz{
+                text-align: center;
+            }
+        </style>
     </head>
     <body>
+    <div id="P_quiz"><img  src="quiz.jpg" alt="quiz" "></div>
+    <h1 id="title">QuizTime(Guest)</h1>
     <div id="button"> <a href="index.php"><input type="button" value="Home"></a>
      <a href="login.php"><input type="button" value="Log in"></a>
      <a href="signup.php"><input type="button" value="Sign up"></a></div>
